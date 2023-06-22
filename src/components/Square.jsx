9c429610cdca6a98b1f21a2b1774c0833e6db0f1
sourabh@32 <sourabhsharma181003@gmail.com>
2023-06-22 22:16:20 +0530

    both tokens are working

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,20 +1,29 @@
-import React, { useContext } from 'react'
-import { redObject,blueObject } from '../constant.js/gameConstants'
-import { gameContext } from '../context/GameContext'
-const Square = ({num,color}) => {
-    const {red,blue,setSelectedToken} = useContext(gameContext)
-    
+import React, { useContext } from "react";
+import { redObject, blueObject } from "../constant.js/gameConstants";
+import { gameContext } from "../context/GameContext";
+const Square = ({ num, color }) => {
+  const {red,blue,random,moveBlue,moveRed} = useContext(gameContext)
+
   return (
     <div className={`square ${color}`}>
-           {num}
+      {num}
 
-{redObject[red.rt1] === num && <button onClick={()=>setSelectedToken("rt1")} className='blueToken'></button>}
-
-{redObject[red.rt2] === num && <button onClick={()=>setSelectedToken("rt2")}  className='blueToken'></button>}
-{blueObject[blue.bt1] === num && <button onClick={()=>setSelectedToken("bt1")}  className='yellowToken'></button>}
-{blueObject[blue.bt2] === num && <button onClick={()=>setSelectedToken("rt2")}  className='yellowToken'></button>}
+      {redObject[red.rt1] === num && (
+         <div className="yellowToken" onClick={()=>moveRed(random,"rt1")}>r1</div>
+      )}
+      {redObject[red.rt2] === num && (
+         <div className="yellowToken" onClick={()=>moveRed(random,"rt2")}>r2</div>
+      )}
+      {blueObject[blue.bt1] === num && (
+         <div className="blueToken" onClick={()=>moveBlue(random,"bt1")}>b1</div>
+      )}
+      {blueObject[blue.bt2] === num && (
+         <div className="blueToken" onClick={()=>moveBlue(random,"bt2")}>b2</div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Square
+export default Square;
+
+
--- src/context/GameContext.jsx
+++ src/context/GameContext.jsx
@@ -1,79 +1,75 @@
 import { useEffect, useState } from "react";
 import { createContext } from "react";
 
-
 export const gameContext = createContext({
-    generateRandom:()=>{},
-    red:0,
-    blue:0,
-    selectedToken:null,
-    setSelectedToken:()=>{}
-})
+  generateRandom: () => {},
+  red: 0,
+  blue: 0,
+  selectedToken: null,
+  setSelectedToken: () => {},
+  setSelectonPhase:()=>{},
+  moveRed:()=>{},
+  moveBlue:()=>{}
+});
 
+const GameProvider = ({ children }) => {
+  const [selectedToken, setSelectedToken] = useState(null);
+  const [red, setRed] = useState({ rt1: 0, rt2: 0 });
+  const [blue, setBlue] = useState({ bt1: 0, bt2: 0 });
+  const [redTurn, setRedTurn] = useState(true);
+  const [random,setRandom] = useState(0)
 
-
-
-
-const GameProvider = ({children}) => {
-  const [selectedToken,setSelectedToken] = useState(null)
-  const [selectionPhase,setSelectonPhase] = useState(true)
-  const [red,setRed] = useState({rt1:0,
-  rt2:0})
-  const [blue,setBlue] = useState({bt1:0,
-    bt2:0})
-  const [redTurn,setRedTurn] = useState(true)
-  
   const generateRandom = () =>{
-    const random = Math.floor(Math.random()*6+1)
+      const random = Math.floor(Math.random() * 6 + 1);
     
-     if(redTurn ){
-      console.log(selectedToken)
+      setRandom(random)
+  }
 
-      if(  selectedToken === "rt1"){
-        setRed(prev => ({...prev,rt1:prev.rt1 +random}))
-        setSelectedToken(null)
-      }
-      else if(selectedToken === "rt2"){
-        setRed(prev => ({...prev,rt1:prev.rt2 +random}))
-        setSelectedToken(null)
-        
-      }
+
+  const moveRed = (random,token) => {
+    if(redTurn){
+
+    
+    if (token === "rt1") {
+      setRed((prev) => ({ ...prev, rt1: prev.rt1 + random }));
+      setRedTurn(false);
+  
+    } else if (token === "rt2") {
+      setRed((prev) => ({ ...prev, rt2: prev.rt2 + random }));
+      setRedTurn(false);
       
+    }
+    }
+};
+
+const moveBlue = (random,token) => {
+  if(!redTurn){
+
+    if (token === "bt1") {
+      setBlue((prev) => ({ ...prev, bt1: prev.bt1 + random }));
+      setRedTurn(true);
       
-      setRedTurn(!redTurn)
-     }
-     else{
-      console.log(selectedToken)
-      if(selectedToken === "bt1"){
-        setBlue(prev => ({...prev,bt1:prev.bt1 +random}))
-        setSelectedToken(null)
-      }
-      else if(selectedToken === "bt2"){
-        setBlue(prev => ({...prev,bt2:prev.bt2 +random}))
-        setSelectedToken(null)
-      }
+    } else if (token === "bt2") {
+      setBlue((prev) => ({ ...prev, bt2: prev.bt2 + random }));
+      setRedTurn(true);
       
-      
-      setRedTurn(!redTurn)
-     }
-   }
+    }
+  }
+};
+  
 
+  useEffect(() => {
+    console.log(random);
+    console.log(redTurn)
+  }, [random]);
 
-   useEffect(()=>{
-  console.log(red,blue)
-   },[red,blue])
+  const value = { generateRandom, red, blue,random,moveRed,moveBlue };
+  return <gameContext.Provider value={value}>{children}</gameContext.Provider>;
+};
 
-
-const value = {generateRandom,red,blue,selectedToken,setSelectedToken}
-  return (
-    <gameContext.Provider value={value}>
-        {children}
-    </gameContext.Provider>
-  )
-}
+export default GameProvider;
 
 
 
 
 
-export default GameProvider
